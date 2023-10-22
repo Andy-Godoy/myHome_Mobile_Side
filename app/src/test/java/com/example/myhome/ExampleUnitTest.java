f3b6397475d7e19cc00e009883b628885122d602
gfalasco <gfalasco@uade.edu.ar>
2023-10-22 18:39:57 -0300

    Se quita la aplicación con kotlin y se reemplaza por el entregable.

--- app/src/test/java/com/example/myhome/ExampleUnitTest.java
+++ app/src/test/java/com/example/myhome/ExampleUnitTest.java
@@ -1,17 +1,17 @@
-package com.example.myhome
+package com.example.myhome;
 
-import org.junit.Test
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Assert.*
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-class ExampleUnitTest {
+public class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
     }
 }
