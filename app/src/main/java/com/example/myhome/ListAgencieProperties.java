83b40ed462163404c6bb575c640ca803be0c1ed1
gfalasco <gfalasco@uade.edu.ar>
2023-10-28 13:46:54 -0300

    Layout para el listado de las propiedades del publico en general (falta pulir) y programar la clase Java ListUserProperties. Se suben también archivos unversioned, y layout bottom_bar_agencies (barra inferior para el alta de propiedades)

---
+++ app/src/main/java/com/example/myhome/ListAgencieProperties.java
@@ -0,0 +1,16 @@
+package com.example.myhome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+public class ListAgencieProperties extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_agencie_properties);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myhome/ListUserProperties.java
@@ -0,0 +1,16 @@
+package com.example.myhome;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListUserProperties extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_user_properties);
+
+
+    }
+}
