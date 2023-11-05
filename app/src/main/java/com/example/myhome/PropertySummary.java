f2ff059308a31f4eab4d47186d8bd5a40818fb16
LetoMarcelo <letomarcelo1@gmail.com>
2023-11-05 15:10:35 -0300

    Feature (new propetie) : Spinner Amenities, retrofit para registro de agencias y login de agencias. se crearon clases MyHome, PropertySummar.

---
+++ app/src/main/java/com/example/myhome/PropertySummary.java
@@ -0,0 +1,85 @@
+package com.example.myhome;
+
+public class PropertySummary {
+    private Long propertyId;
+    private Long agencyId;
+    private Integer propertyPrice;
+    private Integer propertyDimension;
+    private Integer propertyBedroomQuantity;
+    private String propertyDescription;
+    private String propertyAddress;
+    private String propertyNeighbourhood;
+    private String propertyCity;
+
+    public Long getPropertyId() {
+        return propertyId;
+    }
+
+    public void setPropertyId(Long propertyId) {
+        this.propertyId = propertyId;
+    }
+
+    public Long getAgencyId() {
+        return agencyId;
+    }
+
+    public void setAgencyId(Long agencyId) {
+        this.agencyId = agencyId;
+    }
+
+    public Integer getPropertyPrice() {
+        return propertyPrice;
+    }
+
+    public void setPropertyPrice(Integer propertyPrice) {
+        this.propertyPrice = propertyPrice;
+    }
+
+    public Integer getPropertyDimension() {
+        return propertyDimension;
+    }
+
+    public void setPropertyDimension(Integer propertyDimension) {
+        this.propertyDimension = propertyDimension;
+    }
+
+    public Integer getPropertyBedroomQuantity() {
+        return propertyBedroomQuantity;
+    }
+
+    public void setPropertyBedroomQuantity(Integer propertyBedroomQuantity) {
+        this.propertyBedroomQuantity = propertyBedroomQuantity;
+    }
+
+    public String getPropertyDescription() {
+        return propertyDescription;
+    }
+
+    public void setPropertyDescription(String propertyDescription) {
+        this.propertyDescription = propertyDescription;
+    }
+
+    public String getPropertyAddress() {
+        return propertyAddress;
+    }
+
+    public void setPropertyAddress(String propertyAddress) {
+        this.propertyAddress = propertyAddress;
+    }
+
+    public String getPropertyNeighbourhood() {
+        return propertyNeighbourhood;
+    }
+
+    public void setPropertyNeighbourhood(String propertyNeighbourhood) {
+        this.propertyNeighbourhood = propertyNeighbourhood;
+    }
+
+    public String getPropertyCity() {
+        return propertyCity;
+    }
+
+    public void setPropertyCity(String propertyCity) {
+        this.propertyCity = propertyCity;
+    }
+}
