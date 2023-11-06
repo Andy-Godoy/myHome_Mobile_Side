0e7149e3fb2263e901d73b76b1a90e5ac7288ec9
nguzmanphysis <nguzman@physis.com.ar>
2023-11-05 21:54:21 -0300

    (Menu-Pantallas) - Se crea el componente de menu, barra inferior (mis propiedades, agregar propiedad, perfil) - Se agrega el MenuHandler que indica a que pantalla ir.

---
+++ app/src/main/java/com/example/myhome/MenuHandler.java
@@ -0,0 +1,29 @@
+package com.example.myhome;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.MenuItem;
+
+public class MenuHandler {
+    public static void handleMenuItemClick(Context context, MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.action_home) {
+            if (!item.isChecked()) {
+                Intent intent = new Intent(context, ListAgencieProperties.class);
+                context.startActivity(intent);
+                Log.d("MenuHandler", "Home");
+            }
+            // Lógica para abrir la ventana correspondiente a "Mis propiedades" desde aquí
+        } else if (itemId == R.id.action_add) {
+            Intent intent=new Intent(context, NewProperties.class);
+            context.startActivity(intent);
+            // Lógica para abrir la ventana correspondiente a "Agregar propiedad" desde aquí
+        } else if (itemId == R.id.action_profile) {
+//            Intent intent=new Intent(context, AgenciesProfile.class);
+//            context.startActivity(intent);
+            // Lógica para abrir la ventana correspondiente a "Perfil" desde aquí
+        }
+    }
+}
