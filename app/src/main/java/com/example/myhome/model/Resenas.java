9974d80922dddaa47ab0b1ef96b560385454e35e
LetoMarcelo <letomarcelo1@gmail.com>
2023-11-26 19:49:43 -0300

    Feature (new propetie) : Ajuste errores, cree "ListAgencyReviews", se hicieron 2 refactors de "MenuHandlerUsurio" y "PerfilInmobiliaria" y se pusieron adentro de Front. Se actualizó el manifest con la nueva clase.

--- app/src/main/java/com/example/myhome/model/Resenas.java
+++ app/src/main/java/com/example/myhome/model/Resenas.java
@@ -1,4 +1,50 @@
 package com.example.myhome.model;
 
 public class Resenas {
+    private Long reviewId;
+    private String userName;
+    private String photo;
+    private Integer reviewScore;
+    private String reviewComment;
+
+
+    public Long getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(Long reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public Integer getReviewScore() {
+        return reviewScore;
+    }
+
+    public void setReviewScore(Integer reviewScore) {
+        this.reviewScore = reviewScore;
+    }
+
+    public String getReviewComment() {
+        return reviewComment;
+    }
+
+    public void setReviewComment(String reviewComment) {
+        this.reviewComment = reviewComment;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
 }
