ab08d022f7167a93ba1b7cd16bba8d666a6f9224
nguzmanphysis <nguzman@physis.com.ar>
2023-11-28 21:43:48 -0300

    (inmobiliarias-usuario-propiedades) - Se agreg la pantalla de lista de favoritos de usuario.

---
+++ app/src/main/java/com/example/myhome/Front/ListFavoriteProperties.java
@@ -0,0 +1,136 @@
+package com.example.myhome.Front;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.asksira.loopingviewpager.LoopingViewPager;
+import com.example.myhome.Api.PropertyApi;
+import com.example.myhome.Ignore.ImageSliderAdapter;
+import com.example.myhome.Interfaces.PropertiesCallback;
+import com.example.myhome.Network.NetworkUtils;
+import com.example.myhome.R;
+import com.example.myhome.model.FiltersDTO;
+import com.example.myhome.model.Properties;
+import com.example.myhome.model.PropertySummary;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListFavoriteProperties extends AppCompatActivity  implements PropertiesCallback {
+
+    private LinearLayout cardConteiner;
+    private List<PropertySummary> properties;
+    private Long userId;
+    private Long agencyId;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_favorite_user);
+
+        // Validamos la conexión a Internet al iniciar la actividad que lo trae de la clase NetworkUtils.java
+        if (NetworkUtils.isNetworkConnected(this)) {
+
+        } else {
+            // muestra mensaje de error si no hay conexión que lo trae de la clase NetworkUtils.java
+            NetworkUtils.showNoInternetMessage(this);
+        }
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Obtenemos el ID del ítem de menú correspondiente a esta actividad
+        int menuItemId = R.id.action_favorite;
+
+        // Marcar el ítem del menú como seleccionado
+        bottomNavigationView.setSelectedItemId(menuItemId);
+
+        // Configurar el listener para los elementos del menú
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            MenuHandlerUsuario.handleMenuItemClick(this, item);
+            return true;
+        });
+
+        cardConteiner = findViewById(R.id.cardContainer);
+
+        FiltersDTO filters = new FiltersDTO();
+        filters.setIsFavourite(true);
+
+        PropertyApi propertyApi = new PropertyApi();
+        properties = propertyApi.verPropiedades(filters, this);
+
+
+//        LoopingViewPager imageSliderSlider = findViewById(R.id.imageSliderSlider);
+//
+//        // Aquí debes obtener la lista de URLs de tus imágenes en el bucket de Azure
+//        List<String> imageUrls = obtenerUrlsDesdeAzure();
+//
+//        // Crear un adaptador para el LoopingViewPager
+//        ImageSliderAdapter imageSliderAdapter = new ImageSliderAdapter(this, imageUrls);
+//
+//        // Establecer el adaptador en el LoopingViewPager
+//        imageSliderSlider.setAdapter(imageSliderAdapter);
+    }
+
+    private List<String> obtenerUrlsDesdeAzure() {
+        // Lógica para obtener las URLs de las imágenes desde tu bucket de Azure
+        // Puedes implementar la lógica específica para tu aplicación aquí
+        List<String> imageUrls = new ArrayList<>();
+        imageUrls.add("https://storagemyhome.blob.core.windows.net/containermyhome/casa1.jpg");
+        imageUrls.add("https://storagemyhome.blob.core.windows.net/containermyhome/casa2.jpg");
+        imageUrls.add("https://storagemyhome.blob.core.windows.net/containermyhome/casa3.jpg");
+        // Agrega más URLs según sea necesario
+        return imageUrls;
+    }
+
+    @Override
+    public void onPropertiesSuccess(List<PropertySummary> properties) {
+        if (properties != null) {
+            for (PropertySummary p : properties) {
+
+                View propertyCard = LayoutInflater.from(this).inflate(R.layout.card_property_user, cardConteiner, false);
+                List<String> imageUrls = obtenerUrlsDesdeAzure();
+
+                ImageSliderAdapter imageSliderAdapter = new ImageSliderAdapter(this, imageUrls);
+
+                ((TextView) propertyCard.findViewById(R.id.propertyPrice)).setText("USD ".concat(p.getPropertyPrice().toString()));
+                ((TextView) propertyCard.findViewById(R.id.propertyAddress)).setText(p.getPropertyAddress());
+                ((TextView) propertyCard.findViewById(R.id.propertyLocation)).setText(p.getPropertyNeighbourhood().concat(", ").concat(p.getPropertyCity()));
+                ((TextView) propertyCard.findViewById(R.id.propertyDimensions)).setText(p.getPropertyDimension().toString().concat(" M2"));
+                ((TextView) propertyCard.findViewById(R.id.propertyRooms)).setText(p.getPropertyBedroomQuantity().toString().concat(" Habitaciones"));
+                ((TextView) propertyCard.findViewById(R.id.propertyDescription)).setText(p.getPropertyDescription());
+                ((LoopingViewPager) propertyCard.findViewById(R.id.imageSliderSlider)).setAdapter(imageSliderAdapter);
+
+                ImageView imageProperty = propertyCard.findViewById(R.id.propertyImage);
+                String imageUrl = "https://static1.sosiva451.com/521961_a/8b07c18b-b15d-4d23-9bf1-e3d4ce2eea5e_small.jpg";
+                Picasso.get().load(imageUrl).into(imageProperty);
+                cardConteiner.addView(propertyCard);
+
+            }
+
+
+        }
+    }
+
+    @Override
+    public void onPropertiesSuccess(Properties propiedad) {
+
+    }
+
+    @Override
+    public void onPropertiesFailure(String errorMessage) {
+
+    }
+
+    @Override
+    public void onPropertiesSuccess(Long propertyId) {
+
+    }
+
+}
