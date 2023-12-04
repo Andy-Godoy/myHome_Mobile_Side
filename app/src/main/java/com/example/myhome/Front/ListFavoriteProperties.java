a5362958682dc5ab693c34298f7640411eafc4ec
nguzmanphysis <nguzman@physis.com.ar>
2023-12-04 00:50:27 -0300

    (inmobiliarias-users-properties_favorite) - Se agrega el filtro por texto.

--- app/src/main/java/com/example/myhome/Front/ListFavoriteProperties.java
+++ app/src/main/java/com/example/myhome/Front/ListFavoriteProperties.java
@@ -3,8 +3,14 @@ package com.example.myhome.Front;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -27,6 +33,7 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListFavoriteProperties extends AppCompatActivity implements PropertiesCallback {
 
@@ -34,6 +41,8 @@ public class ListFavoriteProperties extends AppCompatActivity implements Propert
     private List<PropertySummary> properties;
     private Long userId;
     private Long agencyId;
+
+    private EditText searchEditText;
     private final float TIPO_CAMBIO_PESOS = 1000;
 
     @Override
@@ -63,11 +72,29 @@ public class ListFavoriteProperties extends AppCompatActivity implements Propert
             return true;
         });
 
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        View decorView = getWindow().getDecorView();
+        decorView.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                int rootViewHeight = decorView.getHeight();
+                int keypadHeight = rootViewHeight - decorView.findViewById(android.R.id.content).getHeight();
+                if (keypadHeight > rootViewHeight * 0.15) {
+                    // Teclado visible, ocultar BottomNavigationView
+                    bottomNavigationView.setVisibility(View.GONE);
+                } else {
+                    // Teclado oculto, mostrar BottomNavigationView
+                    bottomNavigationView.setVisibility(View.VISIBLE);
+                }
+                return true;
+            }
+        });
+
+        searchEditText = findViewById(R.id.editTextSearchFavorite);
         cardConteiner = findViewById(R.id.cardContainer);
 
         cargarDatos();
-
-
+        setupSearchEditText();
     }
 
     private void cargarDatos() {
@@ -85,9 +112,7 @@ public class ListFavoriteProperties extends AppCompatActivity implements Propert
     private List<String> obtenerUrlsDesdeAzure(String[] propertyImages) {
         List<String> imageUrls = new ArrayList<>();
         if (propertyImages != null && propertyImages.length > 0) {
-            for (String i : propertyImages) {
-                imageUrls.add(i);
-            }
+            Collections.addAll(imageUrls, propertyImages);
 
         } else {
             imageUrls.add("https://storagemyhome.blob.core.windows.net/containermyhome/nodisponible.jpg");
@@ -97,59 +122,7 @@ public class ListFavoriteProperties extends AppCompatActivity implements Propert
 
     @Override
     public void onPropertiesSuccess(List<PropertySummary> properties) {
-        if (properties != null) {
-            for (PropertySummary p : properties) {
-
-                View propertyCard = LayoutInflater.from(this).inflate(R.layout.card_property_user, cardConteiner, false);
-                List<String> imageUrls = obtenerUrlsDesdeAzure(p.getPropertyImages());
-
-                ImageSliderAdapter imageSliderAdapter = new ImageSliderAdapter(this, imageUrls);
-
-                if (((MyHome) this.getApplication()).getUsuario() != null) {
-                    String moneda = ((MyHome) this.getApplication()).getUsuario().getUserCurrencyPreference().toString();
-                    Integer valorPropiedad = (Integer) Math.round(p.getPropertyPrice() * ((moneda.equals("USD")) ? 1 : TIPO_CAMBIO_PESOS));
-                    ((TextView) propertyCard.findViewById(R.id.propertyPrice)).setText(moneda + " " + valorPropiedad);
-                }else{
-                    String moneda = "USD";
-                    Integer valorPropiedad = (Integer) Math.round(p.getPropertyPrice() * ((moneda.equals("USD")) ? 1 : TIPO_CAMBIO_PESOS));
-                    ((TextView) propertyCard.findViewById(R.id.propertyPrice)).setText(moneda + " " + valorPropiedad);
-                }
-
-                ((TextView) propertyCard.findViewById(R.id.propertyAddress)).setText(p.getPropertyAddress());
-                ((TextView) propertyCard.findViewById(R.id.propertyLocation)).setText(p.getPropertyNeighbourhood().concat(", ").concat(p.getPropertyCity()));
-                ((TextView) propertyCard.findViewById(R.id.propertyDimensions)).setText(p.getPropertyDimension().toString().concat(" M2"));
-                ((TextView) propertyCard.findViewById(R.id.propertyRooms)).setText(p.getPropertyBedroomQuantity().toString().concat(" Habitaciones"));
-                ((TextView) propertyCard.findViewById(R.id.propertyDescription)).setText(p.getPropertyDescription());
-                ((LoopingViewPager) propertyCard.findViewById(R.id.imageSliderSlider)).setAdapter(imageSliderAdapter);
-
-                ImageView imageProperty = propertyCard.findViewById(R.id.propertyImage);
-                String imageUrl = p.getAgencyImage();
-                if (imageUrl == null) {
-                    imageUrl = "https://storagemyhome.blob.core.windows.net/containermyhome/nodisponible.jpg";
-                } else {
-                    Picasso.get().load(imageUrl).into(imageProperty);
-                }
-
-                cardConteiner.addView(propertyCard);
-
-
-                propertyCard.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // Obtengo el ID de la propiedad
-                        String propertyId = p.getPropertyId().toString();
-
-                        // Iniciar la actividad DetailProperty y paso el ID como extra
-                        Intent intent = new Intent(ListFavoriteProperties.this, DetailUserProperty.class);
-                        intent.putExtra("propertyId", propertyId);
-                        startActivity(intent);
-                    }
-                });
-
-            }
-
-
-        }
+        actualizarVista(properties);
     }
 
     @Override
@@ -184,6 +157,106 @@ public class ListFavoriteProperties extends AppCompatActivity implements Propert
 
         // Cierra la actividad
         finish();
+    }
+
+    private void setupSearchEditText() {
+
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // No necesario para tu caso
+                Log.d("TAG", "beforeTextChanged: " + charSequence.toString());
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                filterProperties(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Log.d("TAG", "afterTextChanged: " + editable.toString());
+                // No necesario para tu caso
+            }
+        });
+    }
+
+    private void filterProperties(String searchText) {
+        List<PropertySummary> filteredProperties = new ArrayList<>();
+        if(properties!=null){
+            filteredProperties = properties.stream()
+                    .filter(property -> propertyContainsText(property, searchText))
+                    .collect(Collectors.toList());
+        }
+        actualizarVista(filteredProperties);
+    }
+
+    private boolean propertyContainsText(PropertySummary property, String searchText) {
+        // Verifica si la descripción de la propiedad contiene el texto de búsqueda
+        String propertyDescription = property.getPropertyAddress().toLowerCase();
+        return propertyDescription.contains(searchText.toLowerCase());
+    }
+
+    private void actualizarVista(List<PropertySummary> properties) {
+        cardConteiner.removeAllViews();
+
+        if (properties != null) {
+
+            if(this.properties == null){
+                this.properties = properties;
+            }
+
+            for (PropertySummary p : properties) {
+
+                View propertyCard = LayoutInflater.from(this).inflate(R.layout.card_property_user, cardConteiner, false);
+                List<String> imageUrls = obtenerUrlsDesdeAzure(p.getPropertyImages());
+
+                ImageSliderAdapter imageSliderAdapter = new ImageSliderAdapter(this, imageUrls);
+
+                if (((MyHome) this.getApplication()).getUsuario() != null) {
+                    String moneda = ((MyHome) this.getApplication()).getUsuario().getUserCurrencyPreference().toString();
+                    Integer valorPropiedad = (Integer) Math.round(p.getPropertyPrice() * ((moneda.equals("USD")) ? 1 : TIPO_CAMBIO_PESOS));
+                    ((TextView) propertyCard.findViewById(R.id.propertyPrice)).setText(moneda + " " + valorPropiedad);
+                }else{
+                    String moneda = "USD";
+                    Integer valorPropiedad = (Integer) Math.round(p.getPropertyPrice() * ((moneda.equals("USD")) ? 1 : TIPO_CAMBIO_PESOS));
+                    ((TextView) propertyCard.findViewById(R.id.propertyPrice)).setText(moneda + " " + valorPropiedad);
+                }
+
+                ((TextView) propertyCard.findViewById(R.id.propertyAddress)).setText(p.getPropertyAddress());
+                ((TextView) propertyCard.findViewById(R.id.propertyLocation)).setText(p.getPropertyNeighbourhood().concat(", ").concat(p.getPropertyCity()));
+                ((TextView) propertyCard.findViewById(R.id.propertyDimensions)).setText(p.getPropertyDimension().toString().concat(" M2"));
+                ((TextView) propertyCard.findViewById(R.id.propertyRooms)).setText(p.getPropertyBedroomQuantity().toString().concat(" Habitaciones"));
+                ((TextView) propertyCard.findViewById(R.id.propertyDescription)).setText(p.getPropertyDescription());
+                ((LoopingViewPager) propertyCard.findViewById(R.id.imageSliderSlider)).setAdapter(imageSliderAdapter);
+
+                ImageView imageProperty = propertyCard.findViewById(R.id.propertyImage);
+                String imageUrl = p.getAgencyImage();
+                if (imageUrl == null) {
+                    imageUrl = "https://storagemyhome.blob.core.windows.net/containermyhome/nodisponible.jpg";
+                } else {
+                    Picasso.get().load(imageUrl).into(imageProperty);
+                }
+
+                cardConteiner.addView(propertyCard);
+
+                propertyCard.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Obtengo el ID de la propiedad
+                        String propertyId = p.getPropertyId().toString();
+
+                        // Iniciar la actividad DetailProperty y paso el ID como extra
+                        Intent intent = new Intent(ListFavoriteProperties.this, DetailUserProperty.class);
+                        intent.putExtra("propertyId", propertyId);
+                        startActivity(intent);
+                    }
+                });
+
+            }
+
+
+        }
     }
 
 }
